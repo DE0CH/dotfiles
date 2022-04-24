c1f3effcdf7199d0ad74927f4579a728307872c4
Deyao Chen <chendeyao000@gmail.com>
2022-04-24 11:06:50 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-echo $DIR
 
 curl https://github.com/DE0CH.keys >> ${HOME}/authorized_keys
 
