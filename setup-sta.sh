b1595f06bfb0d31a4076b119d63724e0cba9f0ea
Deyao Chen <chendeyao000@gmail.com>
2022-04-24 11:04:53 +0800

    update setup.sh

--- setup-sta.sh
+++ setup-sta.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+echo $DIR
+
 cd ${HOME}
 git clone https://github.com/Homebrew/brew .linuxbrew
 .linuxbrew/bin/brew update --force --quiet
@@ -14,16 +25,21 @@ git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cd ${HOMEBREW_PREFIX}/bin
-ln -s gcc gcc-11
-ln -s g++ g++-1
-ln -s cpp cpp-11
-ln -s c++ c++-11
+ln -s gcc-11 gcc 
+ln -s g++-11 g++ 
+ln -s cpp-11 cpp 
+ln -s c++-11 c++
 
-
+cd ${HOME}
+REL=$(realpath --relative-to="${HOME}" "${DIR}")
+if [ -f "${HOME}/.zshrc" ]; then 
+    echo "~/.zshrc exists, moving it to ~/.zshrc.backup"
+    mv ${HOME}/.zshrc ${HOME}/.zshrc.backup
+fi
+ln -s ${DIR}/.zshrc .zshrc
+ln -s ${DIR}/.p10k.zsh .p10k.zsh
 
 cd ${HOME}
 rm -rf .ssh
--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
-DIR=${PWD}
-cd ${HOME}
-REL=$(realpath --relative-to="${HOME}" "${DIR}")
-ln -s ${DIR}/.zshrc .zshrc
-ln -s ${DIR}/.p10k.zsh .p10k.zsh
+#!/usr/bin/env bash
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+echo $DIR
+
 
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -10,3 +16,13 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+cd ${HOME}
+REL=$(realpath --relative-to="${HOME}" "${DIR}")
+if [ -f "${HOME}/.zshrc" ]; then 
+    echo "~/.zshrc exists, moving it to ~/.zshrc.backup"
+    mv ${HOME}/.zshrc ${HOME}/.zshrc.backup
+fi
+ln -s ${DIR}/.zshrc .zshrc
+ln -s ${DIR}/.p10k.zsh .p10k.zsh
