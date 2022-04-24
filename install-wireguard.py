22427b296759314667d9fae6287d1a6849cdafcb
Deyao Chen <chendeyao000@gmail.com>
2022-04-24 12:25:45 +0800

    add wireguard installers

--- install-wireguard.py
+++ install-wireguard.py
@@ -79,6 +79,7 @@ ip_network = ipaddress.ip_network((args.ip+"/24"), strict=False)
 i = 0
 for ip in ip_network:
     if i != 0 and ip != args.ip:
-        print(ip_network[2])
+        print(ip)
+        break
     i += 1
 
