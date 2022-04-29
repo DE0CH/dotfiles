30bb6937cde37d403e2d1f27ba67343196af9e57
Deyao Chen <chendeyao000@gmail.com>
2022-04-30 00:26:12 +0800

    update gen key

--- setup-keys.sh
+++ setup-keys.sh
@@ -8,10 +8,25 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 
+backup () {
+  FILE=$1
+  if [ -f "${FILE}" ]; then
+    BACKUP="${FILE}.backup"
+    while [ -f "${BACKUP}" ]; do
+      BACKUP="${BACKUP}.backup"
+    done
+    FILE_REL=$(realpath --relative-to="${HOME}" "${FILE}")
+    BACKUP_REL=$(realpath --relative-to="${HOME}" "${BACKUP}")
+    echo "~/${FILE_REL} exists, moving it to ~/${BACKUP_REL}"
+    mv ${FILE} ${BACKUP}
+  fi 
+}
 
+backup ${HOME}/.ssh/authorized_keys
+backup ${HOME}/.ssh/id_rsa
 curl https://github.com/DE0CH.keys > ${HOME}/.ssh/authorized_keys
 
 gpg --batch --gen-key ${DIR}/gen-key-script
-ssh-keygen -b 2048 -t rsa -f ${HOME}/.ssh/id_ras -q -N ""
+ssh-keygen -b 2048 -t rsa -f ${HOME}/.ssh/id_rsa -q -N ""
 gpg --armor --export "Deyao Chen"
 cat ${HOME}/.ssh/id_ras.pub
