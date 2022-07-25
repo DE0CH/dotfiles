b40350bbccc8ca53f1d16c654833ac8fed45ccbf
Deyao Chen <chendeyao000@gmail.com>
2022-07-25 17:27:06 +0800

    update aws to install gh

--- setup-aws.sh
+++ setup-aws.sh
@@ -28,4 +28,10 @@ curl -fsSL https://test.docker.com -o test-docker.sh
 sh test-docker.sh
 usermod -aG docker deyaochen
 
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+apt update
+apt install gh
+
 su deyaochen -s /home/deyaochen/dotfiles/setup-zsh.sh
