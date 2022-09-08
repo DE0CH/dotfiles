d4495aa7a0f151deebedf0b26f15060676fd7de6
Deyao Chen <chendeyao000@gmail.com>
2022-09-08 13:56:06 +0100

    update launch ec2 to include london

--- launch-ec2.py
+++ launch-ec2.py
@@ -3,9 +3,11 @@ import subprocess
 import json
 import os 
 import argparse
+import shlex
 
 parser = argparse.ArgumentParser("")
 parser.add_argument('arch', choices=["64", "arm"], default="64")
+parser.add_argument('region', choices=["london", "hk"])
 args = parser.parse_args()
 
 os.chdir(os.path.dirname(__file__))
@@ -19,13 +21,24 @@ if launched_ec2:
   instance_id = launched_ec2[0]
 else:
   if args.arch == "64":
-    image_id = "ami-01efa0814106ea343"
-    instance_type = "c5.4xlarge"
+    if args.region == "hk":
+      image_id = "ami-01efa0814106ea343"
+      instance_type = "c5.4xlarge"
+    elif args.region == "london":
+      raise NotImplementedError()
   else:
-    image_id = "ami-01ed943edc33bc944"
-    instance_type = "c6g.4xlarge"
-  security_group_id = "sg-44c9992f"
-  subnet_id = "subnet-983cd1f1"
+    if args.region == "hk":
+      image_id = "ami-01ed943edc33bc944"
+      instance_type = "c6g.4xlarge"
+    elif args.region == "london":
+      image_id = "ami-0ba4932cffdd282f7"
+      instance_type = "c6g.4xlarge"
+  if args.region == "hk":
+    security_group_id = "sg-44c9992f"
+    subnet_id = "subnet-983cd1f1"
+  elif args.region == "london": 
+    security_group_id = "sg-d2b73eb7"
+    subnet_id = "subnet-90b3e4f9"
   storage = 64
   volumes = [{
     "DeviceName": "/dev/xvda",
@@ -33,6 +46,8 @@ else:
       "VolumeSize": storage,
     },
   }]
+  print(image_id, instance_type, security_group_id,subnet_id)
+  print(' '.join(map(shlex.quote, ['aws', 'ec2', 'run-instances', '--image-id', image_id, '--instance-type', instance_type, '--key-name', 'default-ssh', '--security-group-ids', security_group_id, '--subnet-id', subnet_id, "--block-device-mappings", json.dumps(volumes)])))
   launch_output = subprocess.check_output(['aws', 'ec2', 'run-instances', '--image-id', image_id, '--instance-type', instance_type, '--key-name', 'default-ssh', '--security-group-ids', security_group_id, '--subnet-id', subnet_id, "--block-device-mappings", json.dumps(volumes)])
   launch_output = json.loads(launch_output)
   instance_id = launch_output["Instances"][0]["InstanceId"]
