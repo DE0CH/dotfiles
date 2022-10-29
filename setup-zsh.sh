161d9ae7612b3a1d412cfc18914132f0b270aa56
Deyao Chen <chendeyao000@gmail.com>
2022-10-30 00:36:39 +0100

    zsh script exit on error

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
